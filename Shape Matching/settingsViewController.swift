945fa839f3dff91be5151dd571f3fa94f9157792
Jamison Bunge <JamisonBunge@outlook.com>
2019-01-19 19:55:46 -0500

    updating github to reflect local changes, very close to publishing

---
+++ Shape Matching/settingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  settingsViewController.swift
+//  Shape Matching
+//
+//  Created by Jamison Bunge on 1/9/19.
+//  Copyright © 2019 Jamison Bunge. All rights reserved.
+//
+
+import Foundation
